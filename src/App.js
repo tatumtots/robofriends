9eb857517e3d8d2dadcbd1d9c461435e381d2370
Tatum Bray <tatumrbray@gmail.com>
2021-07-01 17:40:01 -0600

    searchField and styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import React from "react"
+
 import CardList from "./CardList"
+import SearchBox from "./SearchBox"
 import { robots } from "./robots"
 
-function App() {
-  return (
-    <div>
-      <CardList robots={robots} />
-    </div>
-  )
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      robots: robots,
+      searchfield: "",
+    }
+  }
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value })
+  }
+
+  render() {
+    const filteredRobots = this.state.robots.filter((robots) => {
+      return robots.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase())
+    })
+
+    return (
+      <div className='tc'>
+        <h1>RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList robots={filteredRobots} />
+      </div>
+    )
+  }
 }
 
 export default App
